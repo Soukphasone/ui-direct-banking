e51f47298a6b874c4cd1538172fd993bec7e57c9
soukphasone <soukphasone2001@gmail.com>
2024-12-13 17:12:23 +0700

    test

--- vite.config.ts
+++ vite.config.ts
@@ -4,17 +4,29 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [
+//     vue(),
+//   ],
+//   resolve: {
+//     alias: {
+//       '@': fileURLToPath(new URL('./src', import.meta.url))
+//     }
+//   },
+//   server: {
+//     host: '0.0.0.0',
+//     port: 5410
+//   }
+// })
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+  build: {
+    chunkSizeWarningLimit: 1000, // Adjust size limit
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['vue', 'element-plus'] // Split vendor libraries
+        }
+      }
     }
-  },
-  server: {
-    host: '0.0.0.0',
-    port: 5410
   }
-})
+});
