8b95fa0b396abc177ac94c6db1e05b043701c574
soukphasone <soukphasone2001@gmail.com>
2024-12-13 16:49:02 +0700

    fix-router

--- src/App.vue
+++ src/App.vue
@@ -5,21 +5,3 @@
 <script setup lang="ts">
 // import { RouterLink, RouterView } from 'vue-router'
 </script>
-
-<style>
-@font-face {
-  font-family: "NotoSansLao";
-  /* src: url('../fonts/Noto_Sans_Lao/static/NotoSansLao-Regular.ttf') format('truetype'); */
-  src: url("./fonts//Noto_Sans_Lao/static/NotoSansLao-Regular.ttf")
-    format("truetype");
-}
-
-p,
-h2,
-h3,
-span,
-div {
-  margin: 0px;
-  font-family: "NotoSansLao", Avenir, Helvetica, Arial, sans-serif;
-}
-</style>
